64f2d44950133ed4c2db828e79e1b8d036c7e89b
JeremyRojasF <jeremy.rojas.f@gmail.com>
2023-06-08 23:19:23 -0500

    Leer csv y agregar carpeta sql

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
-
+import pandas as pd
+import io
 app = FastAPI()
 
 
 @app.get("/move_historical_data")
 def move_historical_data():
-    return "Se inserto la data historica de forma exitosa !"
+    csv_path = "csv_files/hired_employees.csv"
+    df = pd.read_csv(csv_path)
+    
+    with open('sql/create_table.sql', 'r') as file:
+        create_table = file.read()
+
+    return f"Se inserto {df.shape[0]} registros de forma exitosa !" + create_table
 
 @app.get("/backup")
 def backup():
-    return "Se hizo el backup de forma exitosa !"
+    return "Se hizo el backup de forma exitosa !" 
 
 @app.get("/restore")
 def restore():
---
+++ sql/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS hired_employees(
+    id integer,
+    name varchar(100),
+    datetime varchar(100),
+    department_id integer,
+    job_id integer
+);
