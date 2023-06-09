ce7d77eee519538fa136edaa02dc25d1c4d90277
JeremyRojasF <jeremy.rojas.f@gmail.com>
2023-06-08 23:59:45 -0500

    Probar conexion con PostgreSQL

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from fastapi import FastAPI
 import pandas as pd
 import io
+import psycopg2
+
+host = "localhost"
+user = "postgres"
+password = "postgres"
+database = "postgres"
+
 app = FastAPI()
 
 
 @app.get("/move_historical_data")
 def move_historical_data():
-    csv_path = "csv_files/hired_employees.csv"
-    df = pd.read_csv(csv_path)
-    
-    with open('sql/create_table.sql', 'r') as file:
-        create_table = file.read()
 
-    return f"Se inserto {df.shape[0]} registros de forma exitosa !" + create_table
+    try:
+        csv_path = "csv_files/hired_employees.csv"
+        df = pd.read_csv(csv_path)
+        
+        with open('sql/create_table.sql', 'r') as file:
+            create_table = file.read()
+
+
+
+        conn = psycopg2.connect(host = host,user = user,password = password,database = database)
+        cursor = conn.cursor()
+        cursor.execute("select version()" )
+        row = cursor.fetchone()
+        #return f"Se inserto {df.shape[0]} registros de forma exitosa !"
+        return row
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/backup")
 def backup():
